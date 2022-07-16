ec25791619319a139aeaa4b91aaadf67bea6975e
Bria <brialinza@gmail.com>
2022-07-16 01:48:58 -0600

    Cat sprites and new Player scripting
    a

---
+++ GMTK2022GameJam/Assets/_Bria/Scripts/Player1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public CharacterController controller;
+    //public Material playerMaterial;
+    public Vector3 playerVelocity;    
+    public float playerSpeed = 2.0f;
+    public PlayerKick1 PlayerKick;
+
+    private Texture texRight;
+    private Texture texLeft;
+
+    private void Start()
+    {
+        if(controller == null) controller = gameObject.GetComponent<CharacterController>();
+        if(PlayerKick == null) PlayerKick = gameObject.GetComponent<PlayerKick1>();
+
+        //texRight = (Texture) Resources.Load("Assets/_Bria/BriaSprites/gamejamebirb-export.png");
+        //texLeft = (Texture)Resources.Load("Assets/_Bria/BriaSprites/flip_gamejamebirb-export.png");
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (move.x > 0)
+        {
+            //playerMaterial.SetTexture("_MainTex", texRight);
+        }
+        else
+        {
+            //playerMaterial.SetTexture("_MainTex", texLeft);
+        }
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("KICK");
+            PlayerKick.RequestKick();
+        }
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
---
+++ GMTK2022GameJam/Assets/_Bria/Scripts/PlayerKick1.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKick1 : MonoBehaviour
+{
+
+    public float KickRange = 2;
+    public List<Dice> AllDice;
+
+    public void CollectAllCloseDice()
+    {
+        AllDice.Clear();
+
+        RaycastHit[] hitArray = Physics.SphereCastAll(transform.position,KickRange,transform.forward,8);
+        if (hitArray.Length != 0)
+        {
+            foreach (var hit in hitArray)
+            {
+                if (hit.collider.gameObject.CompareTag("Die"))
+                {
+                    AllDice.Add(hit.collider.gameObject.GetComponent<Dice>());
+                }
+            }
+        }
+    }
+    public void Update()
+    {
+        CollectAllCloseDice();
+        OutLineManagement();
+    }
+    public void OutLineManagement()
+    {
+        foreach (var item in AllDice)
+        {
+            float dist = Vector3.Distance(transform.position, item.transform.position);
+            if (dist < KickRange && item.rb.velocity.magnitude < 0.1f)
+            {
+                item.SetOutlineVisable(true);
+            }
+            else
+            {
+                item.SetOutlineVisable(false);
+            }
+        }
+    }
+    public void RequestKick()
+    {
+        foreach (var item in AllDice)
+        {
+            float dist = Vector3.Distance(transform.position, item.transform.position);
+            if (dist < KickRange)
+            {
+                // kick the ball
+                if (item.rb.velocity.magnitude < 0.1f)
+                {
+                    item.Power = 50;
+                }
+                else
+                {
+                    item.Power = 100;
+                }
+                item.Kick(transform.position);
+            }
+        }
+    }    
+}
