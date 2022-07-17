77dbf36a6eb0a7fa62274ef91b9c281eff360891
Bria <brialinza@gmail.com>
2022-07-16 18:58:12 -0600

    enemy slime !!!! (navmesh!! )
    hi :3

---
+++ GMTK2022GameJam/Assets/_Bria/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+enum EnemyState
+{
+    FollowPlayer,
+    FollowTarget,
+    ZeroGravity
+}
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Player1 player;
+
+    public Vector3 followTarget;
+
+    private EnemyState enemyState;
+
+    void Start()
+    {
+        enemyState = EnemyState.FollowPlayer;
+
+        agent.updateRotation = false;
+
+        followTarget = new Vector3(0, -2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (enemyState)
+        {
+            case EnemyState.FollowPlayer:
+                agent.destination = player.transform.position;
+                break;
+            case EnemyState.FollowTarget:
+                agent.destination = followTarget;
+                break;
+        }
+    }
+}
