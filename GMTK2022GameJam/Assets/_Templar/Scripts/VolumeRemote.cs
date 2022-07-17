e9985e3101f29efe65b0be4d74b8f2dbbceaba45
Templar <carlegos101@gmail.com>
2022-07-17 04:13:22 -0500

    Added Volume Remote

---
+++ GMTK2022GameJam/Assets/_Templar/Scripts/VolumeRemote.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeRemote : MonoBehaviour
+{
+    public AudioListener audioListener;
+    public float VolumePercent;
+
+    public GameObject PositivePos;
+    public GameObject NegativePos;
+
+
+    void Start()
+    {
+        if (audioListener != null) audioListener = FindObjectOfType<AudioListener>();
+    }
+    public void Visualizer()
+    {
+
+    }
+    void Update()
+    {
+        Visualizer();
+
+
+    }
+    public void ChangeVolume()
+    {
+
+    }
+}
